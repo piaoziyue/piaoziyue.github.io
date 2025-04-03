bf35f92f9812ae5f67279664dc1cf310473d0ca1
piaoziyue <ziyue.piao@mail.mcgill.ca>
2025-04-03 11:46:11 -0400

    manually update news

--- src/data/news.ts
+++ src/data/news.ts
@@ -20,15 +20,15 @@ export const newsData: News[] = [
     date: "January 2025",
     title: "Our CHI paper was conditionally accepted at CHI 2025!",
     link: "https://chi2025.acm.org/",
-  },
-  {
-    date: "December 2024",
-    title: "A Graduate Student Consortium paper was accepted at TEI 2025!",
-    link: "https://tei.acm.org/2025/",
-  },
-  {
-    date: "November 2024",
-    title: "I got IEEE TCH Innovation in Haptics Research Grant!",
-    link: "https://www.ieee-ras.org/haptics/activities/research-proposals",
   }
+  // {
+  //   date: "December 2024",
+  //   title: "A Graduate Student Consortium paper was accepted at TEI 2025!",
+  //   link: "https://tei.acm.org/2025/",
+  // },
+  // {
+  //   date: "November 2024",
+  //   title: "I got IEEE TCH Innovation in Haptics Research Grant!",
+  //   link: "https://www.ieee-ras.org/haptics/activities/research-proposals",
+  // }
 ];
